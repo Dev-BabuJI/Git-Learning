2fee1e64990b738209722488f5d564c32757f394
Sachin Vishwakarma <skvishwakarma5757@gamil.com>
2025-06-08 11:23:42 +0530

    Latest Changes

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ console.log(login);
 
 const footer = "footer added in out website";
 console.log(footer);
+const payment = "Integreated the payment gatway";
+console.log(payment);
+
+console.log("Latest Update");
+console.log("latest Update2");
